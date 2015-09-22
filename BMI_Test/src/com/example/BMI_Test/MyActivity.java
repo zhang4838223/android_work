ea0db7c30fdeb38a6bff361fb07cf9b4f2864875
xiaojun.zhang <409751089@qq.com>
2015-09-22 17:29:51 +0800

    体重、身高计算BMI值

--- BMI_Test/src/com/example/BMI_Test/MyActivity.java
+++ BMI_Test/src/com/example/BMI_Test/MyActivity.java
@@ -1,11 +1,17 @@
 package com.example.BMI_Test;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -26,33 +32,72 @@ public class MyActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        setViews();
+
+        setEvents();
+    }
+
+    private void setEvents() {
+        cal_btn.setOnClickListener(MyActivity.this);
+    }
+
+    private void setViews() {
         height = (EditText)findViewById(R.id.height);
         weight = (EditText)findViewById(R.id.weight);
         result = (TextView)findViewById(R.id.result);
         suggest = (TextView)findViewById(R.id.suggestion);
         cal_btn = (Button)findViewById(R.id.calculate_btn);
-
-        cal_btn.setOnClickListener(MyActivity.this);
     }
 
     @Override
     public void onClick(View v) {
 
-        DecimalFormat df = new DecimalFormat("0.00");
-        Double h = Double.valueOf(height.getText().toString())/100;
-        Double w = Double.valueOf(weight.getText().toString());
-        double value = w / (h*h);
+        try {
+            DecimalFormat df = new DecimalFormat("0.00");
+            Double h = Double.valueOf(height.getText().toString()) / 100;
+            Double w = Double.valueOf(weight.getText().toString());
+            double value = w / (h * h);
+            Log.v("BMI value: ",value+"");
 
-        result.setText("your BMI value is : "+ df.format(value));
+            result.setText("your BMI value is : " + df.format(value));
 
-        if(value > 25){
-            suggest.setText(R.string.advice_heavy);
-        }else if(value<20){
-            suggest.setText(R.string.advice_light);
-        }else {
-            suggest.setText(R.string.advice_average);
+            if (value > 25) {
+                suggest.setText(R.string.advice_heavy);
+            } else if (value < 20) {
+                suggest.setText(R.string.advice_light);
+            } else {
+                suggest.setText(R.string.advice_average);
 
+            }
+        }catch (Exception e){
+            Toast.makeText(MyActivity.this,Toast.LENGTH_SHORT,R.string.toast_error).show();
         }
+        openDialogView();
 
+    }
+
+    /**
+     * 弹出对话框
+     */
+    private void openDialogView() {
+        new AlertDialog.Builder(MyActivity.this)
+                .setTitle(R.string.dialog_title)
+                .setMessage(R.string.dialog_message)
+                .setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(MyActivity.this,Toast.LENGTH_SHORT,R.string.toast_error).show();
+                    }
+                })
+                .setNegativeButton(R.string.dialog_homepage, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //跳转，访问网址
+                        Uri uri = Uri.parse("http://www.baidu.com/");
+                        Intent in = new Intent(Intent.ACTION_VIEW,uri);
+                        startActivity(in);
+                    }
+                })
+                .show();
     }
 }
