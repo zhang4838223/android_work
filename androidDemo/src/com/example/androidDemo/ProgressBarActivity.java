b7b9b5f37d64cc54ec31161d91796eea4ca348f2
xiaojun.zhang <409751089@qq.com>
2015-10-21 14:28:13 +0800

    进度条演示

---
+++ androidDemo/src/com/example/androidDemo/ProgressBarActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProgressBarActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.progress_bar);
+        setTitle("progress bar");
+
+    }
+}
